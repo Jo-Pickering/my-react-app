afa5cc960d84f01cdf3ec3fccf57d951b3289b90
Jo Pickering <joanna.r.pickering@gmail.com>
2024-05-17 11:55:28 +0100

    Add React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./components/Root/Root";
+import routes from "./routes";
 import "./assets/css/index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Root />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
